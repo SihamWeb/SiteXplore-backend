488717fcb56a953a633ccc05f73101255bfa678a
SihamWeb <siham.charef@etu.unilim.fr>
2024-02-26 17:15:51 +0100

    main: Configuration Sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import * as process from "process";
+
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+      ConfigModule.forRoot(),
+      SequelizeModule.forRoot({
+        dialect: 'mysql',
+        host: process.env.DB_HOST,
+        port: 3306,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
+        models: [],
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
