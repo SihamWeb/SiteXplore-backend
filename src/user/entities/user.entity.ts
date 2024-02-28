8398f6d8f5b8828242fad37ba50ee0e4936a3c5f
SihamWeb <siham.charef@etu.unilim.fr>
2024-02-28 22:54:24 +0100

    authentication: Creation of user entity

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,30 @@
-export class User {}
+import {AutoIncrement, Column, Model, PrimaryKey} from "sequelize-typescript";
+
+export class User extends Model {
+    @AutoIncrement
+    @PrimaryKey
+    @Column
+    id: number;
+
+    @Column
+    firstname: string;
+
+    @Column
+    lastname: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column
+    password: string;
+
+    @Column
+    isAdmin: boolean;
+
+    @Column({ allowNull: true })
+    profilePicture: string;
+
+    @Column({ type: 'datetime', allowNull: true })
+    lastConnection: Date;
+
+}
