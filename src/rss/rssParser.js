f3c1e53ff698b1eedd321c1a5a262c9f2acb98a6
Valou38 <valentine.maillard@etu.unilim.fr>
2024-02-29 23:50:20 +0100

    branch-integration-rss: récupération données media et ajout des liens

--- src/rss/rssParser.js
+++ src/rss/rssParser.js
@@ -3,12 +3,64 @@ let parser = new Parser();
 
 (async () => {
 
-    let feed = await parser.parseURL('https://www.lefigaro.fr/rss/figaro_histoire.xml');
-    /*console.log(feed.title);*/
+    const parser = require('rss-url-parser')
 
-    const mediaUrls = [];
-    feed.items.forEach(item => {
-        console.log(item['media:content'][0]['media:description'][0]);
-    });
+    var rss_feed =
+        [
+            'https://www.inrap.fr/rss.xml',
+            'https://www.lefigaro.fr/rss/figaro_histoire.xml',
+            'https://www.lemonde.fr/archeologie/rss_full.xml',
+            'https://www.sciencesetavenir.fr/archeo-paleo/rss.xml',
+            'https://services.lesechos.fr/rss/archeologie.xml',
+            'https://archipeldessciences.wordpress.com/category/archeologie/feed/',
+            'https://www.cibpl.fr/rubrique/epaves-et-archeologie/epaves-et-archeologie/feed/',
+            'https://www.cairn.info/rss/rss_encyclo-histoire.xml',
+            'https://histoires-archeologie.lepodcast.fr/rss',
+            'https://radiofrance-podcast.net/podcast09/35099478-7c72-4f9e-a6de-1b928400e9e5/rss_10267.xml',
+            'https://www.youtube.com/feeds/videos.xml?channel_id=UC6Yhg_Phtz-RHX0CBFhU8pA'
+        ]
 
+    for (let r = 0; r < rss_feed.length; r++) {
+
+        console.log('#######################################')
+        console.log('##### Chargement du flux : ' + rss_feed[r]);
+        console.log('#######################################')
+
+        const data = await parser(rss_feed[r]);
+
+        data.forEach((item) => {
+            console.log('#######################################')
+
+            var param = ['title', 'description', 'author', 'pubDate', 'link', 'copyright'];
+            for (let i = 0; i < param.length; i++) {
+                if (item[param[i]]) {
+                    console.log(' - ' + param[i] + ' : ' + item[param[i]]);
+                }
+            }
+
+            // Image miniature youtube
+            if (item.image) {
+                console.log(item.image) ;
+            }
+
+            // Image (ou media audio) enclosure
+            if (item['rss:enclosure']) {
+                console.log(item['rss:enclosure']['@']['url'])
+            }
+
+            // Media content
+             else if (item['media:content']) {
+                var image = ['media:description', 'media:credit', '@'];
+                for (let i = 0; i < image.length; i++) {
+                    if (item['media:content'][image[i]]) {
+                        if (image[i] === '@') {
+                            console.log(image[i] + ' : ' + item['media:content'][image[i]].url);
+                        } else {
+                            console.log(image[i] + ' : ' + item['media:content'][image[i]]['#']);
+                        }
+                    }
+                }
+            }
+        });
+    }
 })();
